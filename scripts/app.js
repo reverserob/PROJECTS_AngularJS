d5a8e8e2c504347c34d57dc320f54ca571372dce
Roberta Randazzo <robertarandazzo@outlook.com>
2015-06-30 01:39:32 +0200

    first
    first

---
+++ scripts/app.js
@@ -0,0 +1,11 @@
+/**
+ * Created by robertarandazzo on 24/06/15.
+ */
+(function() {
+
+    'use strict';
+
+    angular
+        .module('angular-coordinates', ['uiGmapgoogle-maps', 'ngSanitize']);
+
+})();
